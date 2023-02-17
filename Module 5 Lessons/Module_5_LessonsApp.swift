e7156b8633a55646e36dc34c4ad9611404afa429
JCM <JCM>
2023-02-17 15:33:48 -0600

    Initial Commit

---
+++ Module 5 Lessons/Module_5_LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_5_LessonsApp.swift
+//  Module 5 Lessons
+//
+//  Created by Juan Meneses on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Module_5_LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
